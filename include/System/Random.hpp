5ef946fa00ab789bcfed601d0a3a5d4430e83400
mike8699 <mikebuntu68@gmail.com>
2026-08-22 15:07:34 -0400

    ActorNaviBase: vfunc_d4 in progress (93%)
    Everything matches except the instruction scheduling of the sin/cos
    velocity update in states 1/2, where the original computes both
    products before touching mVel. Adds Random::Next(u32 max), the inlined
    form used here (advance, then scale the high word unless max is 0).

--- include/System/Random.hpp
+++ include/System/Random.hpp
@@ -17,6 +17,17 @@ struct Random {
         u64 result   = (mRandomValue >> 32) * (max - min);
         return (result >> 32) + min;
     }
+
+    /**
+     * Generate a random number from 0 (inclusive) to `max` (exclusive)
+     */
+    inline u32 Next(u32 max) {
+        mRandomValue = mAddend + mFactor * mRandomValue;
+        if (max == 0) {
+            return mRandomValue >> 32;
+        }
+        return ((mRandomValue >> 32) * max) >> 32;
+    }
 };
 
 extern Random gRandom;
