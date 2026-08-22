368151038fbbc3d00bcf5051afc5507063ac0793
mike8699 <mikebuntu68@gmail.com>
2026-08-22 13:51:50 -0400

    ActorNaviBase: decompile vfunc_cc

--- include/Unknown/UnkStruct_ov000_020e8b08.hpp
+++ include/Unknown/UnkStruct_ov000_020e8b08.hpp
@@ -6,8 +6,8 @@
 
 struct UnkStruct_ov000_020e8b08 {
     /* 00 */ PAD(0x00, 0x20);
-    /* 20 */ ActorRef mUnk_20[1]; // more than 1, unclear how many
-    /* 28 */ PAD(0x28, 0x55);
+    /* 20 */ ActorRef mUnk_20[4]; // at least 4, unclear how many
+    /* 40 */ PAD(0x40, 0x55);
     /* 55 */ u8 mUnk_55;
     /* 56 */
 };
